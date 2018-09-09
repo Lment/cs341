5723ac7b0903c99c05407845468d5c722c9e7b81
root <strollkim@gmail.com>
2018-09-09 13:17:46 +0000

    Refactor client.c

--- socket/client.c
+++ socket/client.c
@@ -8,34 +8,53 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 
-#define MAX_SIZE 10 * 1000 * 1000 // set max size to 10MB
+#define MAX_SIZE 10 * 1000 * 1000 // 10MB
+#define MAX_DATA 10 * 1000 * 1000 - 8 // 10MB - 8(Header size)
+#define CARRY 0x10000
+#define CAPTURE 0xFFFF
 
-/* TO-DO
-   1. parse cmd and create a message following protocol
-   2. create a socket and connect to a server
-   3. send a message to the server
-   4. receive the message from the server, check and parse the message
- */
+/* References
+   https://beej.us/guide/bgnet/html/single/bgnet.html
+   https://github.com/strollkim/cs341_old/socket/client.c
+   (My private github repository used in Fall, 2017 for the same class, I will provide the access right if required) 
+*/
 
-int calc_checksum (uint8_t *msg, uint32_t length) {
+/* Overall Structure
+   Functions: Define addtional functions used in this program
+   Variables: Declare variables used in this program
+   Part A: Parse a command line and create a message following the given protocal
+   Part B: Create a socket and connect to a server
+   Part C: Send and receive a message to/from server
+*/
+
+
+/* -------------
+   ||Functions||
+   ------------- */
+
+
+// Calculate checksum right before '~' operation required at the end
+int calc_checksum (uint8_t *msg, uint32_t len) {
     uint16_t *tmp = malloc(2);
     int checksum = 0;
-    memset(&msg[length], 0, 1);
-    for (int i = 0;i < length;i = i + 2) {
+    memset(&msg[len], 0, 1);
+    for (int i = 0;i < len;i = i + 2) {
         int carry = 0;
         memcpy(tmp, msg + i, 2);
         checksum = checksum + (int)*tmp;
-        carry = checksum & 0x10000;
-        checksum = checksum & 0xFFFF;
+        carry = checksum & CARRY;
+        checksum = checksum & CAPTURE;
         if (carry) {
-            checksum = checksum +1;
+            checksum = checksum + 1;
         }
     }
     free(tmp);
     return checksum;
 }
 
+// Main function
 int main(int argc, char *argv[]) {
+
     /* header structure
        00  04  08  12  16  20  24  28
        --------------------------------
@@ -43,31 +62,33 @@ int main(int argc, char *argv[]) {
        --------------------------------
        |            length            |
        --------------------------------
-     */
+    */
 
-    /* variables for message */
-    uint8_t op;
-    uint8_t shift;
-    uint32_t length;
-    uint32_t ordered_length;
-    char *data;
-    uint8_t *msg;
-    uint8_t *res;
-    int send_bytes;
-    int recv_bytes;
+    /* -------------
+       ||Variables||
+       -------------
+    */
+    int opt; // Command line option
+    uint8_t op; // Save op from command line
+    uint8_t shift; // Save shift from command line
+    uint32_t length; // Save length of the message
+    uint32_t ordered_length; // Networ-ordered length
+    char *raw_srvr_addr; // Save server address from command line
+    uint16_t port; // Save port from command line
+    struct sockaddr_in srvr_addr; // Create struct for server address
+    int sock_fd; // Socket file descriptor
+    char *data; // Object data following after header
+    uint8_t *msg; // Sent message
+    uint8_t *res; // Received message
+    int send_bytes; // Bytes sent
+    int recv_bytes; // Byte received
 
-    /* variables for socket api */
-    int sock_fd;
-    char *raw_srvr_addr;
-    struct sockaddr_in srvr_addr;
-    uint16_t port;
+    /* ----------
+       ||Part A||
+       ----------
+    */
 
-    /* others */
-    int opt;
-
-    /* parse the command line
-       ex. ./client -h 143.248.111.222 -p 1234 -o 0 -s 5
-     */
+    // Parse the command line (./client -h 143.248.111.222 -p 1234 -o 0 -s 5)
     while ((opt = getopt(argc, argv, "h:p:o:s:")) != -1) {
         switch (opt) {
             case 'h':
@@ -86,24 +107,29 @@ int main(int argc, char *argv[]) {
         }
     }
 
-    /* create a socket and connect to a server */
+    // Create a socket and connect to a server
     if ((sock_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-        perror("Fail to create socket\n");
+        // perror("Socket failed\n");
         return -1;
     } else {
-        /* set server address */
+        // Set server address
         memset(&srvr_addr, 0, sizeof(srvr_addr));
         srvr_addr.sin_family = AF_INET;
         srvr_addr.sin_addr.s_addr = inet_addr(raw_srvr_addr);
-        fprintf(stderr, "%s\n", raw_srvr_addr);
+        // fprintf(stderr, "%s\n", raw_srvr_addr);
         free(raw_srvr_addr);
         srvr_addr.sin_port = htobe16(port);
 
         if (connect(sock_fd, (struct sockaddr *)&srvr_addr, sizeof(srvr_addr)) < 0) {
-            perror("Fail to connect to server\n");
+            // perror("Connect failed\n");
             return -1;
         }
     }
+
+    /* ----------
+       ||Part B||
+       ----------
+    */
 
     /* TO-DO
        1. stdin message
@@ -111,34 +137,36 @@ int main(int argc, char *argv[]) {
        3. send to server
        4. receive the response
      */
+
     while (1) {
         int total_send = 0;
         int total_recv = 0;
         int data_len = 0;
         int eof_flag = 0;
-        data = malloc(MAX_SIZE - 8);
+        data = malloc(MAX_DATA);
         msg = malloc(MAX_SIZE);
         res = malloc(MAX_SIZE);
-        while (data_len < MAX_SIZE - 8) {
+
+        while (data_len < MAX_DATA) {
             int cur_char = getchar();
             if (cur_char == EOF) {
                 if (feof(stdin)) {
                     if (data_len == 0) {
-                        fprintf(stderr, "%s\n", "Reach EOF with zero data");
+                        // fprintf(stderr, "%s\n", "No data");
                         free(msg);
                         free(res);
                         free(data);
                         close(sock_fd);
                         return 0;
                     } else {
-                        fprintf(stderr, "%s\n", "eof with some body");
+                        // fprintf(stderr, "%s\n", "Normal EOF");
                         eof_flag = 1;
                         break;
                     }
                 }
 
                 if (ferror(stdin)) {
-                    perror("Fail to get body from stdin\n");
+                    // perror("Stdin failed\n");
                     free(msg);
                     free(res);
                     free(data);
@@ -149,6 +177,7 @@ int main(int argc, char *argv[]) {
             data[data_len] = (char)cur_char;
             data_len++;
         }
+
         /* header structure
            00  04  08  12  16  20  24  28
            --------------------------------
@@ -160,39 +189,42 @@ int main(int argc, char *argv[]) {
 
         memcpy(&msg[0], &op, 1);
         memcpy(&msg[1], &shift, 1);
-        memset(&msg[2], 0, 2); // checksum place
+        memset(&msg[2], 0, 2);
 
-        length = 8 + data_len;
+        length = data_len + 8;
         ordered_length = htobe32(length);
         memcpy(&msg[4], &ordered_length, 4);
-
         memcpy(&msg[8], data, data_len);
 
-        /* checksum */
         int checksum = calc_checksum (msg, length);
         checksum = (~checksum);
-            memcpy(&msg[2], &checksum, 2);
+        memcpy(&msg[2], &checksum, 2);
  
+        /* ----------
+           ||Part C||
+           ----------
+        */
+
         while (total_send < length) {
             if ((send_bytes = send(sock_fd, msg, (size_t)length, 0)) < 0) {
-                perror("Fail to send message\b");
+                // perror("Send failed\n");
                 free(msg);
                 free(res);
                 free(data);
                 close(sock_fd);
                 return -1;
             }
+
             total_send = total_send + send_bytes;
-            fprintf(stderr,"total: %d/%d, send: %d, left: %d\n", total_send, length, send_bytes, length - total_send);
+            // fprintf(stderr,"total: %d/%d, send: %d, left: %d\n", total_send, length, send_bytes, length - total_send);
         }
  
         uint8_t *total_res = malloc(MAX_SIZE);
  
         while (total_recv < total_send) {
-                 fprintf(stderr, "%d\n", total_recv);
- 
+            // fprintf(stderr, "%d\n", total_recv);
             if ((recv_bytes = recv(sock_fd, res, (size_t)length, 0)) < 0) {
-                perror("Fail to receive message\n");
+                // perror("Recv failed\n");
                 free(msg);
                 free(res);
                 free(total_res);
@@ -203,12 +235,12 @@ int main(int argc, char *argv[]) {
  
             memcpy(&total_res[total_recv], res, recv_bytes);
             total_recv = total_recv + recv_bytes;
-            fprintf(stderr,"total: %d/%d, recv: %d, left: %d\n", total_recv, length, recv_bytes, total_send - total_recv);
- 
+            // fprintf(stderr,"total: %d/%d, recv: %d, left: %d\n", total_recv, length, recv_bytes, total_send - total_recv);
         }
  
         memset(&total_res[total_recv], 0, 3);
         int recv_checksum = calc_checksum (total_res, total_recv);
+
         if (recv_checksum - 0xFFFF) {
             free(msg);
             free(res);
@@ -230,11 +262,8 @@ int main(int argc, char *argv[]) {
         if (eof_flag) {
             break;
         }
-
     }
 
     close(sock_fd);
-
     return 0;   
-
 }
